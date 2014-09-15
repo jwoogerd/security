23f93e0b78e7115443e1f3de80706727b3292355
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-09-15 20:20:44 +0000

    Fix up packet sniffing, printing

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -17,20 +17,19 @@ def live_sniff(iface)
         if pkt.is_tcp?
             flags = pkt.tcp_flags
             proto = pkt.proto[-1]
-            payload = Base64.encode64(pkt.payload)
+            payload = pkt.payload
     	    if flags.urg == 0 && flags.ack == 0 && flags.psh == 0 &&
-    	       flags.rst == 0 && flags.syn == 0 && flags.fin == 0
-                print_incident("NULL scan", pkt.ip_saddr, proto, payload)
+    	       flags.rst == 0 && flags.syn == 0 && flags.fin == 0 && pkt.tcp_dport == 80 
+                print_incident("NULL scan is detected", pkt.ip_saddr, proto, payload)
     	    end
-    	    if flags.urg == 1 && flags.ack == 1 && flags.psh == 1 &&
-    	       flags.rst == 1 && flags.syn == 1 && flags.fin == 1 
-                print_incident("XMAS scan", pkt.ip_saddr, proto, payload)
+    	    if flags.urg == 1 && flags.psh == 1 && flags.fin == 1 && pkt.tcp_dport == 80
+                print_incident("XMAS scan is detected", pkt.ip_saddr, proto, payload)
     	    end
             if pkt.payload[/4\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/] ||
                 pkt.payload[/5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/] ||
                 pkt.payload[/6011(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/] ||
                 pkt.payload[/3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}/]
-                print "ALERT: Credit card leaked in the clear from #{pkt.ip_saddr} (#{proto}) (#{payload})!"
+                print_incident("Credit card leaked in the clear", pkt.ip_saddr, proto, payload)
             end
         end
     end
@@ -56,24 +55,24 @@ def check_line(line)
     incident[:protocol] = incident[:payload].lines(" ")[-1][/[A-Z]*/]
 
     if incident[:status][/4[0-9][0-9]/]
-        print_incident("HTTP error", incident[:source], incident[:protocol],
+        print_incident("HTTP error is detected", incident[:source], incident[:protocol],
             incident[:payload])
     end
     if incident[:nmap]
-        print_incident("Nmap scan", incident[:source], incident[:protocol],
+        print_incident("Nmap scan is detected", incident[:source], incident[:protocol],
             incident[:payload])
     end
     if incident[:payload][/\\x[0-9, A-F][0-9, A-F]/] && incident[:payload][/^((?!HTTP).)*$/]
         # TODO: protocol for shellcode?
-        print_incident("Shellcode", incident[:source], "TCP",
+        print_incident("Shellcode is detected", incident[:source], "TCP",
             Base64.encode64(incident[:payload]))
     end
 end
 
 $incident_number = 0
 def print_incident(attack, source, protocol, payload)
-    print "#{$incident_number}. ALERT: #{attack} is detected from " +
-          "#{source} (#{protocol}) (#{payload})!\n"
+    print "#{$incident_number}. ALERT: #{attack} " +
+          "from #{source} (#{protocol}) (#{payload})!\n"
     $incident_number += 1
 end
 
