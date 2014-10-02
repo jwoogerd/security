af5698a413b716833a8bcd4ec1cf10f56b04af49
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-10-02 08:14:29 -0400

    Parse command line, print live stream data

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -7,10 +7,42 @@
 # to run: sudo ruby alarm.rb || sudo ruby alarm.rb -r <web_server_log>
 
 require 'packetfu'
+require 'optparse'
 
-stream = PacketFu::Capture.new(:start => true, :iface => 'en0', :promisc => true)
-sleep 10
-stream.save
-first_packet = stream.array[0]
-print "#{first_packet}"
-# stream.show_live()
+def live_sniff(iface)
+    # capture a live stream of network packets (and print the result, for now)
+    iface ||= 'eth0'
+    cap = PacketFu::Capture.new(:start => true, :iface => iface, :promisc => true)
+    cap.stream.each do |p|
+        pkt = PacketFu::Packet.parse p
+        if pkt.is_ip?
+            packet_info = [pkt.ip_saddr, pkt.ip_daddr, pkt.size, pkt.proto.last]
+            print "#{packet_info}"
+        end
+    end
+end
+
+
+def log_sniff()
+    print "log sniff"
+end
+
+def main()
+    options = {:log => false}
+
+    OptionParser.new do |opts|
+        opts.banner = "Usage: alarm.rb [options]"
+        opts.on("-r", "--file filename") do |filename| 
+            options[:log] = true
+            options[:filename] = filename
+        end
+    end.parse!
+
+    if options[:log]
+        log_sniff() 
+    else 
+        live_sniff('en0')
+    end
+end
+
+main()
