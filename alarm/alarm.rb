4954635999ac21efccc9b98e52ef3bf202076a1b
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-10-06 20:20:36 -0400

    Alert only on first port scanned (not for every port)

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -17,11 +17,11 @@ def live_sniff(iface = 'eth0')
         if pkt.is_tcp?
     	    flags = pkt.tcp_flags
     	    if flags.urg == 0 && flags.ack == 0 && flags.psh == 0 &&
-    	       flags.rst == 0 && flags.syn == 0 && flags.fin == 0 
+    	       flags.rst == 0 && flags.syn == 0 && flags.fin == 0 && pkt.tcp_dport == 0
                 print_incident("NULL scan", pkt.ip_saddr, "TCP", pkt.payload)
     	    end
     	    if flags.urg == 1 && flags.ack == 1 && flags.psh == 1 &&
-    	       flags.rst == 1 && flags.syn == 1 && flags.fin == 1 
+    	       flags.rst == 1 && flags.syn == 1 && flags.fin == 1 && pkt.tcp_dport == 0 
                 print_incident("XMAS scan", pkt.ip_saddr, "TCP", pkt.payload)
     	    end
         end
