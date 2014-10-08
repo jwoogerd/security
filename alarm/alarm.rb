1065cab075a25413e8a2b715547419454ea115a9
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-10-08 10:40:17 -0400

    Get iface dynamically, clean up packet sniffing

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -3,34 +3,34 @@
 # Jayme Woogerd
 # Comp 116 - Security
 # Assignment 2: Incident Alarm
-# October 7, 2014
+# October 8, 2014
 # to run: sudo ruby alarm.rb || sudo ruby alarm.rb -r <web_server_log>
 
 require 'packetfu'
 require 'base64'
 
-def live_sniff(iface = 'eth0')
+def live_sniff(iface)
     # capture a live stream of network packets (and print the result, for now)
     cap = PacketFu::Capture.new(:start => true, :iface => iface, :promisc => true)
     cap.stream.each do |p|
         pkt = PacketFu::Packet.parse p
         if pkt.is_tcp?
             flags = pkt.tcp_flags
+            proto = pkt.proto[-1]
             payload = Base64.encode64(pkt.payload)
-	    print "flags #{flags} source #{pkt.ip_saddr}"
     	    if flags.urg == 0 && flags.ack == 0 && flags.psh == 0 &&
     	       flags.rst == 0 && flags.syn == 0 && flags.fin == 0
-                print_incident("NULL scan", pkt.ip_saddr, "TCP", payload)
+                print_incident("NULL scan", pkt.ip_saddr, proto, payload)
     	    end
     	    if flags.urg == 1 && flags.ack == 1 && flags.psh == 1 &&
     	       flags.rst == 1 && flags.syn == 1 && flags.fin == 1 
-                print_incident("XMAS scan", pkt.ip_saddr, "TCP", payload)
+                print_incident("XMAS scan", pkt.ip_saddr, proto, payload)
     	    end
             if pkt.payload[/4\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/] ||
                 pkt.payload[/5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/] ||
                 pkt.payload[/6011(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/] ||
                 pkt.payload[/3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}/]
-                print "ALERT: Credit card leaked in the clear from #{pkt.ip_saddr} (HTTP) (#{payload})!"
+                print "ALERT: Credit card leaked in the clear from #{pkt.ip_saddr} (#{proto}) (#{payload})!"
             end
         end
     end
@@ -65,7 +65,7 @@ def check_line(line)
     end
     if incident[:payload][/\\x[0-9, A-F][0-9, A-F]/] && incident[:payload][/^((?!HTTP).)*$/]
         # TODO: protocol for shellcode?
-        print_incident("Shellcode", incident[:source], incident[:protocol],
+        print_incident("Shellcode", incident[:source], "TCP",
             Base64.encode64(incident[:payload]))
     end
 end
@@ -81,7 +81,8 @@ def main()
     option = ARGV[0]
     filename = ARGV[1]
     if option == nil
-        live_sniff()
+        iface = PacketFu::Config.new().config[:iface]
+        live_sniff(iface = iface)
     end
     if option == '-r'
         if filename == nil
